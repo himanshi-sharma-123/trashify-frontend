470fe836f1e50bc957bab9b532f66c19b71bd199
Himanshi Sharma <himanshish456@gmail.com>
2023-11-22 23:07:24 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">hello</header>
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import styles from "./Navbar.module.css";
+import {
+  AiOutlineMenu,
+  AiOutlineClose,
+  AiOutlineSearch,
+  AiOutlineUser,
+} from "react-icons/ai";
+import Logo from "../../images/logo1.jpg";
+
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  return (
+    <header className={styles.navbar}>
+      <img className={styles.image} src={Logo} alt="/" />
+      <nav>
+        <ul
+          className={
+            nav ? [styles.menu, styles.active].join(" ") : [styles.menu]
+          }
+        >
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/">About</a>
+          </li>
+          <li>
+            <a href="/">Models</a>
+          </li>
+          <li>
+            <a href="/">Gallery</a>
+          </li>
+          <li>
+            <a href="/">Contact</a>
+          </li>
+          <li>
+            <AiOutlineSearch size={25} style={{ marginTop: "6px" }} />
+          </li>
+          <li>
+            <AiOutlineUser size={25} style={{ marginTop: "6px" }} />
+          </li>
+        </ul>
+      </nav>
+      <div onClick={() => setNav(!nav)} className={styles.mobile_btn}>
+        {nav ? <AiOutlineClose size={25} /> : <AiOutlineMenu size={25} />}
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
