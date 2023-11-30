f8a0bf17a282e34cc450e70fca4bbeaaa7234d61
Himanshi Sharma <himanshish456@gmail.com>
2023-11-30 11:38:56 +0530

    button added

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 import styles from "./Hero.module.css";
 const Hero = () => {
-  return (
-    <div className={styles.hero}>
-      <h1>Hero</h1>
-    </div>
-  );
+  return <div className={styles.hero}>{/* <h1>Hero</h1> */}</div>;
 };
 
 export default Hero;
